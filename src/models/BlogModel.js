092ba8b71e0dbf61d1e7f141126a1db52969a613
longvuduong <dvlongne88@gmail.com>
2024-04-03 17:26:21 +0700

    blog api

--- src/models/BlogModel.js
+++ src/models/BlogModel.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 const blogSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
+    author: { type: String, required: true },
+    typeName: { type: String, required: true },
+    typeSlug: { type: String, required: true },
     content: { type: String, required: true },
+    image: { type: String },
   },
   {
     timestamps: true,
