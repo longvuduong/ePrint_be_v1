8fba0556831ce19b042711828187d61d358495b6
longvuduong <dvlongne88@gmail.com>
2024-04-10 11:06:46 +0700

    typing fix

--- src/index.js
+++ src/index.js
@@ -10,12 +10,8 @@ const app = express();
 const port = process.env.PORT || 3002;
 console.log("🚀 ~ port:", port);
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  })
-);
+app.use(cors());
+
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.json());
